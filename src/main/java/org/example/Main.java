5094af899181f37c99e18a95c64853792d9b5354
Luiz Felipe <lffm1994@gmail.com>
2023-05-25 20:46:42 -0300

    Trabalhando com textos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,21 @@ public class Main {
         System.out.println("Esse é o Screen Match");
         System.out.println("Filme: Top Gun: Maverick");
 
-        // camelCase
-        int anoDeLancamento = 2002;
-        System.out.println("Ano de lançamento " + anoDeLancamento);
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento:" + anoDeLancamento);
         boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8) / 3;
+        //Média calculada pelas 3 notas
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de Lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
     }
 }
