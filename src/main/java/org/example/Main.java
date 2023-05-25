603addabe6ac4ed1846c39812efff1c4ddc050b5
Luiz Felipe <lffm1994@gmail.com>
2023-05-25 20:28:26 -0300

    Atribuindo valores ao filme

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        // camelCase
+        int anoDeLancamento = 2002;
+        System.out.println("Ano de lançamento " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
     }
 }
