55e4b4d756629bcddc139c8cbdec00e5a86ab930
Luiz Felipe <lffm1994@gmail.com>
2023-05-25 21:01:18 -0300

    Convertendo valores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,5 +21,11 @@ public class Main {
                 Ano de Lançamento
                 """ + anoDeLancamento;
         System.out.println(sinopse);
+
+        // código omitido
+
+        int classificacao;
+        classificacao = (int) (media/ 2);
+        System.out.println(classificacao);
     }
 }
